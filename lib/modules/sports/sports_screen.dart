3aeb2733b57ded7d3e05c2f84b4444a456007c32
ahmed3ab3al <ahmedalaaabdalalw9@gmail.com>
2026-04-30 19:11:43 +0300

    get sports data

--- lib/modules/sports/sports_screen.dart
+++ lib/modules/sports/sports_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/cubit/news_cubit.dart';
+import 'package:news_app/cubit/news_states.dart';
+import 'package:news_app/widgets/custom_news_tile.dart';
 
 class SportsScreen extends StatelessWidget {
   const SportsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Bussiness Screen');
+    return BlocConsumer<AppCubit, AppStates>(
+      builder: (BuildContext context, state) {
+        return CustomScrollView(
+          slivers: [
+            SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return CustomNewsTile(
+                  articalModel: AppCubit.get(context).articalsSportsList[index],
+                );
+              }, childCount: AppCubit.get(context).articalsSportsList.length),
+            ),
+          ],
+        );
+      },
+      listener: (BuildContext context, state) {},
+    );
   }
 }
